e25860fcb89a35d0d3d6d91e6c9d6d01f3b5e1f4
h-dobashi <ponyo4869yakata398@gmail.com>
2019-07-12 06:37:45 +0900

    Remove files.
    練習用の"_"から始まるディレクトリとその配下のファイル、その他必要のないファイル/ディレクトリを削除しました

--- _abc128/Atcorder_setup.bat
+++
@@ -1,9 +0,0 @@
-mkdir question_a question_b question_c question_d
-for /d %%a in (question_*) do (
-  cd %%a
-  type nul > main.rb
-  type nul > sample1.txt
-  type nul > sample2.txt
-  type nul > sample3.txt
-  cd ..
-)
--- _divert02/a/main.rb
+++
@@ -1,8 +0,0 @@
-# n = gets.chomp.to_i
-n,k = gets.chomp.split.map(&:to_i)
-
-if k==1
-    puts 0
-else
-    puts n-k
-end
